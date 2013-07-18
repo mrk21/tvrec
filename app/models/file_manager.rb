3c4aa83943933a65b40d6f244bdc219393bc7c95
Yuichi Murata <mrk21info+github@gmail.com>
2013-07-18 20:44:53 +0900

    fixes #180.

--- app/models/file_manager.rb
+++ app/models/file_manager.rb
@@ -1,11 +1,12 @@
 class FileManager
-  INVALID_CHARS_FROM = ':;/\\|,*?"<>()$~'.split(//)
-  INVALID_CHARS_TO = '：；／＼｜，＊？”＜＞（）＄〜'.split(//)
+  INVALID_CHARS_FROM = '!:;/\\|,*?<>()$~\'"[]#♯&%'.split(//)
+  INVALID_CHARS_TO = '！：；／＼｜，＊？＜＞（）＄〜’”［］＃＃＆％'.split(//)
   
   def self.to_filename(name)
     name = NKF.nkf('-m0Z1 -w', name.to_s).strip
     name = name.gsub(/^\.\.$/,'‥')
     name = name.gsub(/^\.$/,'・')
+    name = name.gsub(/\s+/,'_')
     
     INVALID_CHARS_FROM.each_with_index do |c,i|
       name = name.gsub(c, INVALID_CHARS_TO[i])
--- test/models/file_manager_test.rb
+++ test/models/file_manager_test.rb
@@ -3,15 +3,9 @@ require 'test_helper'
 class FileManagerTest < ActiveSupport::TestCase
   test "ファイル名に適したものに変換できること" do
     targets = {
-      ':;/\|,*?"<>()$~' => '：；／＼｜，＊？”＜＞（）＄〜',
-      'ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ' => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+      '!:;/\|,*?<>()$~\'"[]#♯&%' => '！：；／＼｜，＊？＜＞（）＄〜’”［］＃＃＆％',
+      'ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ' => 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
       '０１２３４５６７８９' => '0123456789',
-      
-      '【】' => '【】',
-      '［］' => '[]',
-      '（）' => '（）',
-      '()' => '（）',
-      '＃' => '#',
       
       '.' => '・',
       '..' => '‥',
@@ -32,6 +26,10 @@ class FileManagerTest < ActiveSupport::TestCase
       ' . ' => '・',
       ' ..　 ' => '‥',
       ' 　... ' => '...',
+      
+      'a b   c　　　d
+      
+      e	f	　 	g' => 'a_b_c_d_e_f_g',
     }
     targets.each do |from,to|
       converted = FileManager.to_filename(from)
